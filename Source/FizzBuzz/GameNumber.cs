f93c75141fecf3ebc2356e7f1bbaad3d4791e519
Kevin Main <kevmain@outlook.com>
2015-01-09 09:16:40 +0000

    Initial Commit

---
+++ Source/FizzBuzz/GameNumber.cs
@@ -0,0 +1,12 @@
+﻿namespace FizzBuzz
+{
+    public class GameNumber
+    {
+        public string Value { get; private set; }
+
+        public GameNumber(string value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ Source/FizzBuzz/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start....");
+
+            FizzBuzz(1);
+           
+            Console.WriteLine("Press ESC to stop");
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    // Do something
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+
+        private static void FizzBuzz(int i) //0
+        {
+            if (i <= 100)
+            {
+                GameNumber gameNumber;
+
+                if (i % 15 == 0)
+                {
+                    gameNumber = new GameNumber("FizzBuzz");
+                }
+                else if (i%3 == 0)
+                {
+                    gameNumber = new GameNumber("Fizz");
+                }
+                else if (i%5 == 0)
+                {
+                    gameNumber = new GameNumber("Buzz");
+                }
+                else
+                {
+                    gameNumber = new GameNumber(i.ToString(CultureInfo.InvariantCulture));
+                }
+                Console.WriteLine(gameNumber.Value);
+
+                FizzBuzz(i + 1);
+            }
+        }
+    }
+}
